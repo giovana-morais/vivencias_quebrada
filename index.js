4725842727592e257b7e7e2d5bbe6ce227fe1b1f
Giovana Morais <giovana.vmorais@gmail.com>
2025-06-25 17:08:34 -0400

    feat: popup com imagem

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var map = L.map('map').setView([-23.5, -46.6], 13);
+var map = L.map('map').setView([-23.55, -46.47], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	crossOrigin: true  // Add this line
+	crossOrigin: true
 }).addTo(map);
 
 
-L.marker([-23.5292, -46.6378]).addTo(map).bindPopup("que saudade do café colombiano").openPopup();
+// TODO: ler de um arquivo em vez de adicionar os tooltips manualmente.
+// idealmente, um .yml ou algo que a Ana possa popular.
+// vou pensar na estrutura
+L.marker([-23.5420, -46.4710]).addTo(map).bindPopup("Texto de teste<br><img src='dados/corinthians_itaquera.jpeg' width='200' height='300'/>");
+L.marker([-23.5757, -46.4104]).addTo(map).bindPopup("Texto de teste<br><img src='dados/quadra.jpeg' width='200' height='300'/>");
+L.marker([-23.5679, -46.4189]).addTo(map).bindPopup("Texto de teste<br><img src='dados/riacho.jpeg' width='200' height='300'/>");
